befb536b4cfcb34652cfac01f25375fcf35f3286
mdtahsin00 <sikdartahsin420@gmail.com>
2026-09-11 21:46:45 +0600

    Hero Section Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
-import Navbar from './component/Navbar'
+import HeroSection from './component/Hero'
+import Navbar from './component/navbar'
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <HeroSection></HeroSection>
     </>
   )
 }
---
+++ src/component/Hero.tsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+  return (
+    <section>
+      <div className="pt-20 flex justify-between container mx-auto">
+        <div>
+          <h1 className="text-[60px] font-bold">Build Your Ideal <br/> <span className="bg-linear-to-r bg-clip-text text-transparent from-orange-600 via-pink-500 to-purple-600"> Development Stack</span></h1>
+          <p className="pt-5 pb-5 text-[18px]">
+           compare
+            them side by side, and put together the stack that fits your next
+            project.
+          </p>
+          <div className="flex gap-5">
+            <button className="bg-linear-to-r from-[#F97316] to-[#EC4899] py-2 px-3 rounded-[10px] text-white">Explore Technologies</button>
+            <button className="border border-[#F1F5F9] rounded-[10px] py-2 px-10">Learn More</button>
+          </div>
+        </div>
+        <div>
+          <img className="mx-auto w-full max-w-lg" src="/public/assets/banner-stack.png" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+}
